de098e26c7a1ad9b17c84b59bd70e6123682af2e
Islam Atwa <Islam Atwa>
2024-11-02 20:20:52 +0200

    first commit

---
+++ algorithms.js
@@ -0,0 +1,68 @@
+// What is algorithms ?
+    //Algorithm is a step-by-step procedure for solving a problem or accomplishing a task
+
+/*
+
+How Algorithms work ?
+    Algorithms typically follow a logical structure:
+
+    Input: The algorithm receives input data.
+    Processing: The algorithm performs a series of operations on the input data.
+    Output: The algorithm produces the desired output.
+*/
+
+/*
+خواص الخوارزميات ؟ 
+    
+    - Clear and Unambiguous
+    - Well-defined Inputs
+    - Well defined Outputs
+    - Finiteness: The algorithm must be finite, i.e. it should terminate after a finite time.
+    -The algorithm must be simple, generic, and practical, 
+    such that it can be executed using reasonable constraints and resources.
+
+    -  must be just plain instructions that can be implemented in any language,
+        and yet the output will be the same, as expected.
+*/
+
+/*
+Why Wee need algorithms ?
+
+    - Solving Proplem 
+    - Optimizing solution 
+    - Automating tasks: Algorithms can automate repetitive or complex tasks, saving time and effort.
+*/
+
+/*
+Examples of Algorithms
+
+    - Sorting algorithms: Merge sort, Quick sort, Heap sort
+    - Searching algorithms: Linear search, Binary search, Hashing
+    - Graph algorithms: Dijkstra's algorithm, Prim's algorithm, Floyd-Warshall algorithm
+    - String matching algorithms: Knuth-Morris-Pratt algorithm, Boyer-Moore algorithm
+*/
+
+/*
+How to create Algorithms 
+    - Define the proplem 
+    - Design the algoritms
+    - Implement the algorithm
+    - Test and debug 
+*/
+
+/* 
+Types of Algorithms
+    1. Searching and Sorting Algorithms
+    2. Greedy Algorithms
+    3. Dynamic Programming Algorithms
+    4. Pattern Searching Algorithms
+    5. Backtracking Algorithm
+    6. Divide and Conquer Algorithm
+    7. Geometric Algorithm
+    8. Mathematical Algorithms
+    9. Bitwise Algorithms
+    10. Graph Algorithms
+    11. Randomized Algorithms
+    12. Branch and Bound Algorithms
+
+*/
