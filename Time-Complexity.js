6af74417b08072ee183aa9023b02d8c64c47954d
Islam Atwa <Islam Atwa>
2024-11-07 19:03:19 +0200

    file about Time Complexity

---
+++ Time-Complexity.js
@@ -0,0 +1,24 @@
+/*
+Why we need time complexity ?
+    - Performance Prediction:
+    - Resource Management
+    - Optimization
+    - Scalability
+    - Comparative Analysis
+    - Avoiding Inefficiency
+
+*/
+
+/*
+    cases in complexities:
+
+    1- Best case complexity: The best-case scenario for an algorithm is the scenario in which the algorithm performs the minimum amount of work
+    (e.g. takes the shortest amount of time, uses the least amount of memory, etc.).
+    أفضل سيناريوا للخوارزمية هي الخوارزمية التي تؤدي الحد الأدني من العمل اي أنها تأخذ 
+    اي أنها تأخد أقصر قدر من الوقت وأقل مساحة من الذاكرة 
+
+    2 - Best case complexity: The best-case scenario for an algorithm is the scenario in which the algorithm performs the minimum amount of work 
+    (e.g. takes the shortest amount of time, uses the least amount of memory, etc.).
+    أسوء سيناريو للخوارزمية هو السيناريو التي تؤدي فيه الخوارزمية  أقصي قدر من العمل 
+    أي أنها تأخذ أكبر مدة من الوقت وأكبر مساحة من الذاكرة 
+*/
