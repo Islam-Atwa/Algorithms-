b5ed3d2fdaab8f52938c96acfb2f9a9f56769da4
Islam Atwa <Islam Atwa>
2024-11-09 18:34:03 +0200

    Asymptotic Notation

---
+++ AsymptoticNotation.js
@@ -0,0 +1,65 @@
+// Asymptotic Notation
+
+/*
+What is Asymptotic Notation ?
+    symptotic Notation is a way to describe the efficiency and performance of algorithms,
+    especially in terms of time and space complexity. It provides a mathematical foundation to analyze how the runtime 
+    or space requirements of an algorithm grow relative to the input size as it becomes very large.
+
+ */
+
+/*
+Types  Asymptotic Notation 
+    1- Big O Notation (O): يصف أسوأ حالة أو الحد الأعلى لمعدل نمو الخوارزمية 
+    2- Omega Notation (Ω):  يمثل أفضل حالة أو الحد الأدنى لمعدل نمو الخوارزمية
+    3- Theta Notation (Θ): يحدد معدل النمو بدقة عندما يكون الحد الأعلى والحد الأدنى متساويين
+*/
+
+/* 
+Rate of Growths (الترتيب من الأفضل للأسوء )
+
+1-
+    Name : Constant 
+    Time Complexity : 1
+    Example : Adding an element to the front of a linked list 
+// ---------------------------------------------
+
+2- 
+    Name : Logarithmic 
+    Time Complexity : log n
+    Example : Finding an element in a sorted array  
+// ---------------------------------------------
+
+3- 
+    Name : Linear 
+    Time Complexity : n
+    Example : Finding an element in a sorted array  
+// ---------------------------------------------
+
+4- 
+    Name : Linear Logarithmic
+    Time Complexity : nlog n
+    Example : Sorting n items by 'Divide and Conquer'  
+
+// ---------------------------------------------
+5- 
+    Name : Quaddratic
+    Time Complexity : n^2
+    Example : Shortest path between 2 nodes in a graph  
+// ---------------------------------------------
+
+6- 
+    Name : Cubic
+    Time Complexity : n^3
+    Example : Matrix Multiplication  
+// ---------------------------------------------
+
+
+7- 
+Name : Exponential
+    Time Complexity : 2^n
+    Example : The Towers Hanoi proplem 
+// ---------------------------------------------
+
+*/
+
